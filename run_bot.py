99687de5fc5fc59e30db9c7686f5f0ebc423a926
RichiePim <72387167+RichiePim@users.noreply.github.com>
2025-02-06 17:06:17 +0000

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -15,10 +15,8 @@ dotenv.load_dotenv()
 
 import logging
 
-from forecasting_tools.forecasting.forecast_bots.main_bot import MainBot
-from forecasting_tools.forecasting.helpers.forecast_database_manager import (
-    ForecastDatabaseManager,
-    ForecastRunType,
+from forecasting_tools.forecasting.forecast_bots.template_bot import (
+    TemplateBot,
 )
 from forecasting_tools.forecasting.questions_and_reports.forecast_report import (
     ForecastReport,
@@ -35,10 +33,13 @@ async def run_forecasts(skip_previous: bool, tournament: int | str) -> None:
     Make a copy of this file called run_bot.py (i.e. remove template) and fill in your bot details.
     This will be run in the workflows
     """
-    forecaster = MainBot(
+    forecaster = TemplateBot(
+        research_reports_per_question=1,
+        predictions_per_research_report=1,
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=skip_previous,
+        use_research_summary_to_forecast=False,
     )
     reports = await forecaster.forecast_on_tournament(
         tournament, return_exceptions=True
@@ -52,18 +53,6 @@ async def run_forecasts(skip_previous: bool, tournament: int | str) -> None:
     minor_exceptions = [
         report.errors for report in valid_reports if report.errors
     ]
-
-    total_cost = 0
-    for report in valid_reports:
-        total_cost += report.price_estimate if report.price_estimate else 0
-        await asyncio.sleep(5)
-        try:
-            ForecastDatabaseManager.add_forecast_report_to_database(
-                report, ForecastRunType.REGULAR_FORECAST
-            )
-        except Exception as e:
-            logger.error(f"Error adding forecast report to database: {e}")
-    logger.info(f"Total cost estimated: {total_cost}")
 
     if exceptions:
         raise RuntimeError(
